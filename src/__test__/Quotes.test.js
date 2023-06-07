1f72eff8d3408e034ca8bb04cff4abc1f176a549
danielsafari143 <danielsafari143@gmail.com>
2023-06-07 16:15:51 +0200

    Update

--- src/__test__/Quotes.test.js
+++ src/__test__/Quotes.test.js
@@ -1,9 +1,9 @@
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Quote from '../components/Quotes';
 
 describe('Confirming display of quote', () => {
   it('quote displays', () => {
-    const quoteDisplay = render(<Quote />);
+    const quoteDisplay = renderer.create(<Quote />).toJSON();
     expect(quoteDisplay).toMatchSnapshot();
   });
 });
